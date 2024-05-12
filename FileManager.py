51e0a7059a8afd8f0ed1090e3ce3bdfe388a6740
Simona <157476590+simona-89@users.noreply.github.com>
2024-05-12 15:08:36 +0300

    Added, updated comments, docstring, type hints.

--- FileManager.py
+++ FileManager.py
@@ -1,21 +1,54 @@
 import os
+from typing import Dict, List, Union
 from PyPDF2 import PdfReader
 from ux_functions import print_categories, print_valid_path
 
 
 class FileManager:
+    """
+    Manages files in the directory.\n
+    Checks if dir path is valid.\n
+    Then checks if PDF inside is valid, reads metadata using PyPDF2.\n
+    Property filepath get and set dir path that FileManager instance
+    is working with.\n
+
+    Attr:
+        filepath (str): Dir path FileManager instance is working with.
+    Methohds:
+        is_valid_path(): Performs validity check on directory.
+        is_valid_pdf(): Performs validity check on PDF.
+    """
     def __init__(self):
         self.filepath = ''
 
     @property
-    def filepath(self):
+    def filepath(self) -> str:
+        """
+        Gets the value of the _filepath attribute.
+
+        Returns:
+            str: Dir path that FileManager instance working with.
+        """
         return self._filepath
 
     @filepath.setter
-    def filepath(self, filepathname):
+    def filepath(self, filepathname: str) -> None:
+        """
+        Sets the value of the _filepath attribute.
+
+        Args:
+            filepathname (str): Dir path the FileManager instance working with.
+        """
         self._filepath = filepathname
 
-    def is_valid_path(self):
+    def is_valid_path(self) -> bool:
+        """
+        Checks if the filepath is a valid directory and prints a message based 
+        on the result.
+
+        Returns:
+            bool: True if path is valid, False otherwise.
+        """
         is_valid = os.path.isdir(self.filepath)
         if is_valid:
             message = (f"\nFolder is found:\n{self.filepath}\n")
@@ -25,7 +58,14 @@ class FileManager:
         print_valid_path(is_valid, message)
         return is_valid
 
-    def is_valid_pdf(self):
+    def is_valid_pdf(self) -> Dict[str, Union[List[str], str]]:
+        """
+        Checks if PDF in the path dir is valid and
+        prints a categorized list of the PDFs.
+
+        Returns:
+            Dict[str, Union[List[str], str]]: The collected data.
+        """
         result = {"Valid PDFs": [],
                   "Invalid PDFs": []}
 
@@ -49,3 +89,4 @@ class FileManager:
                                  "Otherwise, try again."]
 
         print_categories(result)
+
