db0a08b16cff09be9f1439f48031f0ecdb9b5d9a
Simona <157476590+simona-89@users.noreply.github.com>
2024-05-12 15:10:04 +0300

    Added, updated comments, docstring, type hints.

--- ux_functions.py
+++ ux_functions.py
@@ -1,18 +1,38 @@
 import os
+from typing import Dict, List
 from colorama import Fore, Style, init
+
 
 init(autoreset=True)
 
 
-def print_green(text):
+def print_green(text: str) -> None:
+    """
+    Apply green color on text use colorama.
+
+    Args:
+        text (str): Received text.
+    """
     print(Fore.GREEN + text)
 
 
-def print_red(text):
+def print_red(text: str) -> None:
+    """
+    Apply red color on text use colorama.
+
+    Args:
+        text (str): Received text.
+    """
     print(Fore.RED + text)
 
 
-def print_categories(result):
+def print_categories(result: Dict[str, List[str]]) -> None:
+    """
+    Prints categories and corresponding files.
+
+    Args:
+        result (Dict[str, List[str]]): Key - category, value - list of files.
+    """
     for category, files in result.items():
         # Only print the category if it has file
         if files:
@@ -30,16 +50,35 @@ def print_categories(result):
                         print_red(f" - {file}")
 
 
-def print_valid_path(is_valid, message):
+def print_valid_path(is_valid: bool, message: str) -> None:
+    """
+    Prints message in green if path valid, otherwise - in red.
+
+    Args:
+        is_valid (bool): True if the path is valid, False otherwise.
+        message (str): The message to be printed.
+    """
     if is_valid:
         print_green(message)
     else:
         print_red(message)
 
 
-def in_bold(text):
+def in_bold(text: str) -> str:
+    """
+    Returns given text in bold use colorama.
+
+    Args:
+        text (str): The text to be bolded.
+
+    Returns:
+        str: The bolded text.
+    """
     return f"{Style.BRIGHT}{text}{Style.RESET_ALL}"
 
 
-def clear_screen():
+def clear_screen() -> None:
+    """
+    Clears the temrinal.
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
