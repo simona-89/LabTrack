d45906db0187c0c6bf9e01a9f9bde9da299172bf
Simona <157476590+simona-89@users.noreply.github.com>
2024-05-12 15:08:58 +0300

    Added, updated comments, docstring, type hints.

--- helper_functions.py
+++ helper_functions.py
@@ -1,13 +1,20 @@
 import sys
+from typing import NoReturn
 from ux_functions import in_bold, clear_screen
 
 
-def welcome_message():
+def welcome_message() -> None:
+    """
+    Prints a welcome message.
+    """
     print(in_bold("Welcome to LabTrack"))
     print("Your personal assistant to track your blood test results.\n")
 
 
-def menu():
+def menu() -> None:
+    """
+    Prints main options.
+    """
     print(in_bold("LabTrack is here to help you:"))
 
     menu_options = {
@@ -21,6 +28,9 @@ def menu():
         print(f"{in_bold('[' + key + ']')} {value}")
 
 
-def quit_program():
+def quit_program() -> NoReturn:
+    """
+    Clears the screen and exits the program.
+    """
     clear_screen()
     sys.exit(in_bold("\nThank you for using LabTrack!\n"))
